f76c5c0e147b1a74c38055e26442f2351e6fe1a5
Andrew <522988922@qq.com>
2020-02-07 01:14:39 +0800

    PythonLearn

---
+++ NewPlayer/Day03.py
@@ -0,0 +1,37 @@
+username = input('请输入用户名: ')
+password = input('请输入口令: ')
+# 用户名是admin且密码是123456则身份验证成功否则身份验证失败
+if username == 'admin' and password == '123456':
+    print('身份验证成功!')
+else:
+    print('身份验证失败!')
+
+
+x = float(input('x = '))
+if x > 1:
+    y = 3 * x - 5
+else:
+    if x >= -1:
+        y = x + 2
+    else:
+        y = 5 * x + 3
+print('f(%.2f) = %.2f' % (x, y))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
